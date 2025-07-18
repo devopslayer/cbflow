ca706db91838ba731ab3cc85676b00dd1ddd9deb
devopslayer <layerdevops@gmail.com>
2025-07-19 01:44:02 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import App from "./App.jsx";
+
+inject();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
