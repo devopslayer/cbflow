aad035c581b040be71b1e325fc7591bd70bf397b
devopslayer <layerdevops@gmail.com>
2025-07-19 01:12:07 +0530

    initial commit

--- src/pages/NoPageError/NoPageError.jsx
+++ src/pages/NoPageError/NoPageError.jsx
@@ -1,18 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
 
 function NoPageError() {
   return (
     <div className="container p-5">
-        <div className="text-center">
-            <h1 className="display-4">404 - Page Not Found</h1>
-            <p className="lead">The page you are looking for does not exist.</p>
-            <a href="/" className="btn btn-primary">
-            Go to Home
-            </a>
-        </div>
+      <div className="text-center">
+        <h1 className="display-4">404 - Page Not Found</h1>
+        <p className="lead">The page you are looking for does not exist.</p>
+        <Link to="/cbflow/" className="btn btn-primary">
+          Go to Home
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NoPageError
+export default NoPageError;
